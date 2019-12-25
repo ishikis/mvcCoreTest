e33955be46bb148601f4f179a3753b3c5348f597
ishikis <ishik.is@hotmail.com>
2019-12-25 13:14:02 +0300

    ..

--- Controllers/L04PartitialViewController.cs
+++ Controllers/L04PartitialViewController.cs
@@ -1,19 +1,36 @@
 using CourseApp.Models;
 using CourseApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
 namespace CourseApp.Controllers
 {
     public class L04PartitialViewController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            var categories = new List<Category>()
+            {
+                new Category(){Name="Kategori 1"},
+                new Category(){Name="Kategori 2"},
+                new Category(){Name="Kategori 3"}
+            };
+
+
+            return View(categories);
         }
 
         public IActionResult About()
         {
-            return View();
+            var categories = new List<Category>()
+            {
+                new Category(){Name="Kategori 1"},
+                new Category(){Name="Kategori 2"},
+                new Category(){Name="Kategori 3"}
+            };
 
+
+            return View(categories);
         }
     }
 }
