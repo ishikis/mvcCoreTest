8343384c9b2152f2517e4b4cec28daf2e2d05757
ishikis <ishik.is@hotmail.com>
2020-01-06 11:41:45 +0300

    Validation on controler

--- Controllers/L08ValidationsController.cs
+++ Controllers/L08ValidationsController.cs
@@ -17,7 +17,39 @@ namespace CourseApp.Controllers
         [HttpPost]
         public IActionResult Register(Register model)
         {
-            return View("Completed", model);
+            //Model Binding > ModelStateDictionary
+            //AddModelError Hata Ekleme 
+            //GetValidationState durum kontrolu saglarız
+            //IsValid 
+            if (string.IsNullOrEmpty(model.UserName))
+            {
+                ModelState.AddModelError(
+                    nameof(model.UserName),
+                    "UserName Zorunlu ALan");
+            }
+            if (!model.Email.Contains("@"))
+            {
+                ModelState.AddModelError(
+                    nameof(model.Email),
+                    "Email düzgün girilmedi");
+            }
+            if (model.Password.Length < 6)
+            {
+                ModelState.AddModelError(
+                    nameof(model.Password),
+                    "Parola min 6 karakter olmalı");
+            }
+            if (model.TermsAccepted)
+            {
+                ModelState.AddModelError(
+                    nameof(model.TermsAccepted),
+                    "Kullanım koşullarını kabul etmelisiniz.");
+            }
+
+            if (ModelState.IsValid)
+                return View("Completed", model);
+            else
+                return View(model);
         }
     }
 
